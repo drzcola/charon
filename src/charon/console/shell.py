4dc2c4217bf26c7299096da0ade1851efbcb478c
J R <jose.rb@protonmail.com>
2026-07-02 18:03:07 +0200

    add impacket to pyproject. basic style shell

--- src/charon/console/shell.py
+++ src/charon/console/shell.py
@@ -1,14 +1,25 @@
 from prompt_toolkit import PromptSession
+from prompt_toolkit.formatted_text import FormattedText
+from prompt_toolkit.styles import Style
 
 
 class Shell:
     def __init__(self):
         self.session = PromptSession()
+        self.style = Style.from_dict({"bracket": "#34ebc3 bold", "brand": "#042d7a"})
+
+    def _get_prompt(self):
+        tokens = [
+            ("class:bracket", "["),
+            ("class:brand", "charon"),
+            ("class:bracket", "] "),
+        ]
+        return FormattedText(tokens)
 
     def run(self):
         while True:
             try:
-                line = self.session.prompt("[charon] ")
+                line = self.session.prompt(self._get_prompt, style=self.style)
             except KeyboardInterrupt:
                 continue
             except EOFError:
